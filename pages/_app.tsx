edf07c43b51605446ca9b29d210b6fda5ecad7d3
digitalcoreservices <digitalcoreservices@gmail.com>
2025-03-16 06:19:44 +0530

    Updated code for Netlify deployment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,26 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import dynamic from 'next/dynamic';
 import '../styles/globals.css';
+
+// Dynamically import BrowserRouter with SSR disabled
+const RouterWrapper = dynamic(
+  () => import('react-router-dom').then((mod) => {
+    const { BrowserRouter } = mod;
+    return ({ children }: { children: React.ReactNode }) => <BrowserRouter>{children}</BrowserRouter>;
+  }),
+  { ssr: false }
+);
 
 function MyApp({ Component, pageProps }: { Component: any; pageProps: any }) {
   return (
-    <Router>
-      <Component {...pageProps} />
-    </Router>
+    <>
+      {typeof window === 'undefined' ? (
+        <Component {...pageProps} />
+      ) : (
+        <RouterWrapper>
+          <Component {...pageProps} />
+        </RouterWrapper>
+      )}
+    </>
   );
 }
 
